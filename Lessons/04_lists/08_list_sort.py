2da6d80131ee9a0157e80b4d9d6aa0429916df05
Gulyamovj <gulyamovj@yandex.ru>
2024-05-09 02:10:48 +0500

    09051210

--- Lessons/04_lists/08_list_sort.py
+++ Lessons/04_lists/08_list_sort.py
@@ -1,12 +1,30 @@
-cities = ["Москва", "Санкт-Петербург", "Новосибирск", "Калининград", "Владивосток", "Киев", "Минск"]
-populations = [1200000, 5300000, 160000, 480000, 605000, 2900000, 2000000]
+# cities = ["Москва", "Санкт-Петербург", "Новосибирск", "Калининград", "Владивосток", "Киев", "Минск"]
+# populations = [1200000, 5300000, 160000, 480000, 605000, 2900000, 2000000]
+#
+# sorted_cities = sorted(cities, key=len, reverse=True)
+# populations.sort(reverse=True)
+#
+# print(cities)
+# print(sorted_cities)
+# print(populations)
 
+# # Task
+# import sys
+# products = sys.stdin.read().splitlines()
+# products.sort()
+# print(products)
 
-print(cities)
-print(populations)
+# # Task
+# import sys
+# products = ["апельсины", "Хлеб"]
+# new_products = sys.stdin.read().splitlines()
+# products.extend(new_products)
+# products.sort(key=str.upper)
+# print(products)
 
-cities.sort(key=str.upper)
-populations.sort()
+# Task
 
-print(cities)
-print(populations)
+import sys
+numbs = sys.stdin.read().splitlines()
+numbs.sort(key=str.int)
+print(numbs)
