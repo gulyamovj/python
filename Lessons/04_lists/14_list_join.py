a74b306627502a3ee2d95c38bc6367756293b0af
gulyamovj <92231234+gulyamovj@users.noreply.github.com>
2024-05-30 19:29:18 +0500

    300520241929

--- Lessons/04_lists/14_list_join.py
+++ Lessons/04_lists/14_list_join.py
@@ -1,6 +1,7 @@
 product = ["moloko", "maslo", "smetana", "slivki", "kefir", "sir"]
-
+name = "products"
+name = list(name)
+name
+print(name)
 s_products = "\n".join(product)
 print(s_products)
-
-print(product)
