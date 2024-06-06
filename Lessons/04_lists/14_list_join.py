1c336385aca38d104714d8bcd5b81e6238282faf
gulyamovj <92231234+gulyamovj@users.noreply.github.com>
2024-06-06 17:08:13 +0500

    0606241708

--- Lessons/04_lists/14_list_join.py
+++ Lessons/04_lists/14_list_join.py
@@ -1,7 +1,36 @@
-product = ["moloko", "maslo", "smetana", "slivki", "kefir", "sir"]
-name = "products"
-name = list(name)
-name
-print(name)
-s_products = "\n".join(product)
-print(s_products)
+# # Lesson
+# product = ["moloko", "maslo", "smetana", "slivki", "kefir", "sir"]
+# name = "products"
+# name = list(name)
+# name[0] = "P"
+# name = "".join(name)
+# print(name)
+#
+# s_products = "\n".join(product)
+# print(s_products)
+
+# # Task
+# file_name = input()
+# file_name = file_name.split()
+# file_name = "_".join(file_name)
+# print(file_name)
+
+# # Task
+# pets = input()
+# pets = pets.split(", ")
+# pets.reverse()
+# pets = ", ".join(pets)
+# print(pets)
+
+# # Task Человекопонятный URL
+# url = input()
+# url = url.split()
+# url = "-".join(url)
+# url = url.replace(":","")
+# url = url.strip("?")
+# print(url.lower())
+
+# Task
+ing = "булочки,пирожки,печенье"
+ing = ing.split(",")
+print(ing)
