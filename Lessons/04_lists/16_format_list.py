f951a3a54606fba7e92230f42e7a9d84d2ec6e39
gulyamovj <92231234+gulyamovj@users.noreply.github.com>
2024-06-11 18:57:48 +0500

    110620241857

--- Lessons/04_lists/16_format_list.py
+++ Lessons/04_lists/16_format_list.py
@@ -11,12 +11,21 @@
 #                    f"Date: {data[2]}"
 #
 # print(product_template)
+#
+# # Task
+# import sys
+#
+# # Список транзакций
+# transactions = sys.stdin.read().splitlines()
+#
+# # Выводим информацию о последней операции
+# print(f"Последняя операция: {float(transactions[-1]):.2f} руб.")
 
-# Task
-import sys
 
-# Список транзакций
-transactions = sys.stdin.read().splitlines()
+# Расчет стоимости товара
+current_price = 6000000
+inflation = 0.0877
+years = 4
 
-# Выводим информацию о последней операции
-print(f"Последняя операция: {float(transactions[-1]):.2f} руб.")
+price = current_price * ((1 + inflation) ** years)
+print(f"Товар на ${current_price:,} через {years} года (лет) будет: ${price:,.0f}")
