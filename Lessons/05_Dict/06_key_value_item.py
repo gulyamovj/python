8b18c893bfec92de8fcdc07cfe3756d202ae9d5d
gulyamovj <92231234+gulyamovj@users.noreply.github.com>
2024-06-17 14:07:57 +0500

    1706241407

--- Lessons/05_Dict/06_key_value_item.py
+++ Lessons/05_Dict/06_key_value_item.py
@@ -55,3 +55,40 @@
 # k = list(colors.keys())
 # k.sort()
 # print(", ".join(k))
+
+# # Task
+# revenue2023 = {
+#    "q1": 50,
+#    "q2": 200,
+#    "q3": 400,
+#    "q4": 300
+# }
+#
+# new_update = input()
+#
+# quarter = new_update[:1]
+# value = int(new_update[2:])
+#
+# revenue2023.update({f"q{quarter}":value})
+# total = sum(list(revenue2023.values()))
+# procent = (value / total) * 100
+# print(f"{procent:.1f}%")
+
+# Task
+
+users = {
+   "user1@domain.com": "nikitos",
+   "user2@domain.com": "black adam",
+   "user3@domain.com": "minion",
+   "user4@domain.com": "lovey",
+   "user5@domain.com": "barbie",
+   "user6@domain.com": "comedy central",
+   "user7@domain.com": "bro",
+   "user8@domain.com": "rapunzel"
+}
+
+name = input().lower()
+
+user_name = list(users.values())
+
+print(name in user_name)
