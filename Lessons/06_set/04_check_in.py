fdbfecbcf02f5b41c04d6335ba60ffbd5cd1a7ad
gulyamovj <92231234+gulyamovj@users.noreply.github.com>
2024-06-19 19:12:05 +0500

    1906241912

---
+++ Lessons/06_set/04_check_in.py
@@ -0,0 +1,10 @@
+# Lesson
+names_list =      ["Oleg", "Nikita", "Yan", "Oleg", "Sofia", "Yan", "Oleg", "Viktor"]
+names_uniq_list = ["Oleg", "Nikita", "Yan", "Sofia", "Viktor"]
+names_set =       {"Oleg", "Nikita", "Yan", "Sofia", "Viktor"}
+
+name = "Yan"
+
+print(name in names_list)
+print(name in names_uniq_list)
+print(name in names_set)
