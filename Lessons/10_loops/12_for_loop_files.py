d566c39ba201c230d9bf9964673f463549aa8c4e
gulyamovj <92231234+gulyamovj@users.noreply.github.com>
2024-07-10 17:57:36 +0500

    1007241757

--- Lessons/10_loops/12_for_loop_files.py
+++ Lessons/10_loops/12_for_loop_files.py
@@ -1,3 +1,15 @@
+# # Task
+# file_name = input()
+#
+# balance_file = open(file_name, "r", encoding="utf-8")
+# result = 0
+#
+# for line in balance_file.readlines():
+#     name, count = line.split(";")
+#     result = result + int(count)
+#
+# print(result)
+
 # Task
 file_name = input()
 
@@ -5,7 +17,10 @@ balance_file = open(file_name, "r", encoding="utf-8")
 result = 0
 
 for line in balance_file.readlines():
-    name, count = line.split(";")
-    result = result + int(count)
+    product, type, amount = line.split(";")
+    if type == "income":
+        result += int(amount)
+    else:
+        result -= int(amount)
 
 print(result)
