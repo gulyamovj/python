940acaa908250093c0bd95cc7eb0e0b210e9b316
gulyamovj <92231234+gulyamovj@users.noreply.github.com>
2024-07-29 19:16:00 +0500

    2908241915

--- Lessons/11_functions/03_functions.py
+++ Lessons/11_functions/03_functions.py
@@ -1,15 +1,22 @@
-# Lesson
-def mult(x, y):
-    if type(x) == str and x.isdigit():
-        x = int(x)
+# # Lesson
+# def mult(x, y):
+#     if type(x) == str and x.isdigit():
+#         x = int(x)
+#
+#     if type(y) == str and y.isdigit():
+#         x = int(y)
+#
+#     if type(x) not in [int, float] or type(y) not in [int, float]:
+#         raise TypeError
+#     return x * y
+#
+# print(mult(2, 7))
+# print(mult(2.5, 7.12))
+# print(mult("5", "3"))
 
-    if type(y) == str and y.isdigit():
-        x = int(y)
+# Task
+def calc_money(price, hours):
+    hour, minute = hours
+    total = (hour + (minute / 60)) * price
 
-    if type(x) not in [int, float] or type(y) not in [int, float]:
-        raise TypeError
-    return x * y
-
-print(mult(2, 7))
-print(mult(2.5, 7.12))
-print(mult("5", "3"))
+    return f"{total:.0f}"
